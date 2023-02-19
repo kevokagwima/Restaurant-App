598a996e798bcfcc42f6a272893d42fb03782a63
kevin <you@example.com>
2023-02-19 22:30:56 +0300

    Updated Files'

--- Order/views.py
+++ Order/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from Inventory.models import Category, Item
+from Inventory.models import Category, Item, User_Profile
+from django.contrib.auth.models import User
 
 def home(request):
   categories = Category.objects.all()
   items = Item.objects.all()
+  user = request.user
+  user_profile = User_Profile.objects.filter(user=request.user).first()
   context = {
     'categories': categories,
-    'items': items
+    'items': items,
+    'user': user,
+    'user_profile': user_profile
   }
 
   return render(request, "order/home.html", context)
